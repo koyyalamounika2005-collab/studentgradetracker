2b942c6ebc6cea7f23d4fc3fca6c6154053a9d8e
Koyyala Mounika <koyyalamounika2005@gmail.com>
2025-12-25 19:09:39 +0530

    Update and rename README.md to CodeAlpha_studentgradetracker.java

--- CodeAlpha_studentgradetracker.java
+++ CodeAlpha_studentgradetracker.java
@@ -1,2 +1,2 @@
-# studentgradetracker
+CodeAlpha_studentgradetracker
 The student grade tracker is a java based application designed to input, manage and analyse student academic scores  features: input and store student grades. manage data using arrays or arraylist
